86b736c6baba11863ff8591e5a44f5848141f243
soongu <naong526@naver.com>
2025-08-05 09:41:52 +0900

    Initial commit

---
+++ src/main/java/com/spring/toyproject/SpringToyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.toyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringToyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringToyProjectApplication.class, args);
+    }
+
+}
