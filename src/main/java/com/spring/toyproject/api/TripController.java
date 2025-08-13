2c300ac1e8e35989247e35be043d29cb6c6ec2a6
soongu <naong526@naver.com>
2025-08-13 11:16:09 +0900

    Jwt 인증 필터 및 여행 생성 API 개선
    - `JwtAuthenticationFilter`: 주석 수정 및 가독성 향상
    - `TripController`: 스프링 시큐리티로 인증된 사용자 정보 처리 추가 (`@AuthenticationPrincipal`)
      - `username` 매개변수 추가, 로그 및 서비스 호출에서 사용자 정보 활용
    - 환경 설정(`application.yml`): 데이터베이스 URL 변경 (`toy_project` → `toy_project_clone`)

--- src/main/java/com/spring/toyproject/api/TripController.java
+++ src/main/java/com/spring/toyproject/api/TripController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,10 +29,12 @@ public class TripController {
     @PostMapping
     public ResponseEntity<?> createTrip(
            @RequestBody @Valid TripRequest request
+           // 스프링 시큐리티 컨텍스트에서 인증된 사용자의 정보를 가져옴
+           , @AuthenticationPrincipal String username
     ) {
-        log.info("여행 생성 API 호출 - 사용자: {}");
+        log.info("여행 생성 API 호출 - 사용자: {}", username);
 
-        Trip response = tripService.createTrip(request, "");
+        Trip response = tripService.createTrip(request, username);
 
         return ResponseEntity.ok()
                 .body(
